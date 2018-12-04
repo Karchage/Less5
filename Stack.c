e8d22183b1aba4b760ff82b2151276913b0eca33
Karchage <45166558+Karchage@users.noreply.github.com>
2018-12-04 10:48:12 +0300

    Write Stack

---
+++ Stack.c
@@ -0,0 +1,44 @@
+﻿#include "Stack.h"
+#include <stdlib.h>
+
+Node* push(Node** pTop, info_t value)
+{
+	Node* newNode = (Node*)malloc(sizeof(Node));
+	newNode->info = value;
+	newNode->prev = *pTop;
+	*pTop = newNode;
+	return newNode;
+}
+
+
+info_t pop(Node** pTop)
+{
+	Node* oldTop = *pTop;
+	info_t info = oldTop->info;
+
+	*pTop = (*pTop)->prev;
+	free(oldTop);
+	return info;
+}
+
+info_t getTopValue(Node* top)
+{
+	return top->info;
+}
+
+bool isEmpty(Node* top)
+{
+	return top == NULL;
+}
+void WriteStack(Node *top)
+{
+		Node *buffer = top;
+		Node *Check = top;
+		info_t info;
+		while(!isEmpty(Check))
+		{
+		std::cout<<Check->info<<"\n";
+		Check = Check->prev;	
+		}
+		*top = *buffer;
+}
